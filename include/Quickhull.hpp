3027ba84ea20741362cdeb83fac780ce3a8a24a3
llikian <pokillian64@gmail.com>
2025-04-23 16:45:54 +0200

    Moved definition out of class scope

--- include/Quickhull.hpp
+++ include/Quickhull.hpp
@@ -22,30 +22,7 @@ struct Quickhull {
         uint C;
     };
 
-    Quickhull(uint pointsAmount, float boundsMin, float boundsMax)
-        : points(pointsAmount), pointsMesh(GL_POINTS) {
-        uint left = 0;
-        uint right = 0;
-        uint bottom = 0;
-        uint top = 0;
-
-        for(uint i = 0 ; i < pointsAmount ; ++i) {
-            points[i] = vec3::random(boundsMin, boundsMax);
-
-            if(points[i].x < points[left].x) { left = i; }
-            if(points[i].x > points[right].x) { right = i; }
-            if(points[i].y < points[bottom].y) { bottom = i; }
-            if(points[i].y > points[top].y) { top = i; }
-
-            pointsMesh.addPosition(points[i]);
-        }
-
-        std::vector<Triangle> faces;
-        faces.emplace_back(top, left, right);
-        faces.emplace_back(bottom, left, right);
-
-        printf("left: %d\nright: %d\nbottom: %d\ntop: %d\n", left, right, bottom, top);
-    }
+    Quickhull(uint pointsAmount, float boundsMin, float boundsMax);
 
     void draw() {
         pointsMesh.draw();
